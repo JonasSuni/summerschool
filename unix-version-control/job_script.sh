1241b6caac62e201348c86328fdb937fb4630297
JonasSuni <jonas.suni@helsinki.fi>
2022-06-26 19:01:07 +0300

    test

--- unix-version-control/job_script.sh
+++ unix-version-control/job_script.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#SBATCH --job-name=training090_day1
+#SBATCH --job-name=training090_vc
 #SBATCH --account=project_2000745
 #SBATCH --partition=test
 #SBATCH --time=00:05:00
 #SBATCH --nodes=1
 #SBATCH --ntasks=24
-#SBATCH --reservation=summerschool
+##SBATCH --reservation=summerschool
 
 srun prog

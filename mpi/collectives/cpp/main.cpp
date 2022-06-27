f9f1114b80228392f4034e2616fe59b15e2ccc68
JonasSuni <jonas.suni@helsinki.fi>
2022-06-27 19:10:21 +0300

    Test scatter

--- mpi/collectives/cpp/main.cpp
+++ mpi/collectives/cpp/main.cpp
@@ -36,11 +36,13 @@ int main(int argc, char *argv[])
     /* TODO: use a single collective communication call (and maybe prepare
      *       some parameters for the call) */
 
-    MPI_Bcast( sendbuf.data() , 2 * NTASKS , MPI_INT , 0 , MPI_COMM_WORLD);
+    //MPI_Bcast( sendbuf.data() , 2 * NTASKS , MPI_INT , 0 , MPI_COMM_WORLD);
+    MPI_Scatter( sendbuf.data() , 2 , MPI_INT , recvbuf.data() , 2 , MPI_INT , 0 , MPI_COMM_WORLD);
 
     /* Print data that was received */
     /* TODO: add correct buffer */
-    print_buffers(printbuf.data(), sendbuf.data(), 2 * NTASKS);
+    //print_buffers(printbuf.data(), sendbuf.data(), 2 * NTASKS);
+    print_buffers(printbuf.data(), recvbuf.data(), 2 * NTASKS);
 
     MPI_Finalize();
     return 0;

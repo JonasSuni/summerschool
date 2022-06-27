17adc6a9299fa6994507dc24850aec2c6bcecd20
JonasSuni <jonas.suni@helsinki.fi>
2022-06-27 09:19:53 +0300

    MPI hello world

--- mpi/hello-world/main.cpp
+++ mpi/hello-world/main.cpp
@@ -5,11 +5,17 @@
 int main(int argc, char *argv[])
 {
     // TODO: say hello! in parallel
-    //int rank,ntasks;
+    int rank,ntasks;
 
     MPI_Init(&argc,&argv);
+    MPI_Comm_size(MPI_COMM_WORLD,&ntasks);
+    MPI_Comm_rank(MPI_COMM_WORLD,&rank);
 
-    std::cout << "Hello world!\n";
+    if (rank==0) {
+        std::cout << "Hello world from rank " << rank << ", total processes = " << ntasks << "\n";
+    } else {
+        std::cout << "Hello world from rank " << rank << "\n";
+    }
 
     MPI_Finalize();
     return 0;

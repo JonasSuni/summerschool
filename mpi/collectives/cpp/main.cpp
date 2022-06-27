1204893daedd5df47dab4ba9fffc159b2dd54e04
JonasSuni <jonas.suni@helsinki.fi>
2022-06-27 19:17:24 +0300

    Test gatherv

--- mpi/collectives/cpp/main.cpp
+++ mpi/collectives/cpp/main.cpp
@@ -37,7 +37,11 @@ int main(int argc, char *argv[])
      *       some parameters for the call) */
 
     //MPI_Bcast( sendbuf.data() , 2 * NTASKS , MPI_INT , 0 , MPI_COMM_WORLD);
-    MPI_Scatter( sendbuf.data() , 2 , MPI_INT , recvbuf.data() , 2 , MPI_INT , 0 , MPI_COMM_WORLD);
+    //MPI_Scatter( sendbuf.data() , 2 , MPI_INT , recvbuf.data() , 2 , MPI_INT , 0 , MPI_COMM_WORLD);
+
+    const int sencounts[4] = {1,1,2,4};
+    const int displs[4] = {0,0,0,0};
+    MPI_Gatherv( sendbuf.data() , 2 * NTASKS , MPI_INT , recvbuf.data() , sencounts , displs , MPI_INT , 1 , MPI_COMM_WORLD);
 
     /* Print data that was received */
     /* TODO: add correct buffer */

56b78204c6b81fdb2096fb43e2f5a3f0414529d2
JonasSuni <jonas.suni@helsinki.fi>
2022-06-29 14:11:09 +0300

    omp stuff

--- hybrid/omp-hello/main.cpp
+++ hybrid/omp-hello/main.cpp
@@ -1,11 +1,16 @@
 #include <cstdio>
 #include <omp.h>
+
 int main()
 {
     printf("Hello world!\n");
+    
+
 #pragma omp parallel
     {
         printf("X\n");
+        printf("Num threads: %d\n",omp_get_num_threads());
+        printf("Thread number: %d\n",omp_get_thread_num());
     }
     return 0;
 }

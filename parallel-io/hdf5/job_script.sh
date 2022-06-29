bd19a4191d4837c01774d14287bccff62729642e
JonasSuni <jonas.suni@helsinki.fi>
2022-06-29 11:21:40 +0300

    hdf5

---
+++ parallel-io/hdf5/job_script.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --job-name=training090_mpi
+#SBATCH --account=project_2000745
+#SBATCH --partition=large
+#SBATCH --time=00:05:00
+##SBATCH --nodes=1
+#SBATCH --ntasks=4
+#SBATCH --cpus-per-task=1
+#SBATCH --reservation=summerschool
+
+srun ./main

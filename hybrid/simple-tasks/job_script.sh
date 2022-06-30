34773ebd18ab291015f02973803039d4eb895d6f
JonasSuni <jonas.suni@helsinki.fi>
2022-06-30 10:56:40 +0300

    fix

--- hybrid/simple-tasks/job_script.sh
+++ hybrid/simple-tasks/job_script.sh
@@ -3,7 +3,6 @@
 #SBATCH --account=project_2000745
 #SBATCH --partition=large
 #SBATCH --time=00:05:00
-#SBATCH --nnodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
 #SBATCH --reservation=summerschool

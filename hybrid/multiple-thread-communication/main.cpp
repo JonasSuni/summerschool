96255e1c2b7192725d5449f783fea9aaf6551c28
JonasSuni <jonas.suni@helsinki.fi>
2022-06-30 09:40:37 +0300

    bug fix

--- hybrid/multiple-thread-communication/main.cpp
+++ hybrid/multiple-thread-communication/main.cpp
@@ -8,7 +8,7 @@ int main(int argc, char *argv[])
     int provided, required=MPI_THREAD_MULTIPLE;
 
     MPI_Init_thread( &argc , &argv , required , &provided);
-    MPI_Comm_rank( MPI_COMM_WORLD , &rank);
+    MPI_Comm_rank( MPI_COMM_WORLD , &mpi_rank);
     MPI_Comm_size( MPI_COMM_WORLD , &ntasks);
 
     #pragma omp parallel private(omp_rank)
@@ -22,7 +22,7 @@ int main(int argc, char *argv[])
             MPI_Send( &sendbuf , ntasks , MPI_INT , 2 , omp_rank , MPI_COMM_WORLD);
         } else {
             MPI_Recv( &recvbuf , ntasks , MPI_INT , 0 , omp_rank , MPI_COMM_WORLD , MPI_STATUS_IGNORE);
-            printf("My MPI rank is %d, my OMP rank is %d, and I received data from OMP rank %d\n",rank,omp_rank,recvbuf[0]);
+            printf("My MPI rank is %d, my OMP rank is %d, and I received data from OMP rank %d\n",mpi_rank,omp_rank,recvbuf[0]);
         }
     }
 

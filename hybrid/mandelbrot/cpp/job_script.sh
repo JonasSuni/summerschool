8f03cc49c0ecb5bce43d344d129f9992c86a90b2
JonasSuni <jonas.suni@helsinki.fi>
2022-06-30 11:03:09 +0300

    omp mandelbrot

---
+++ hybrid/mandelbrot/cpp/job_script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH --job-name=training090_mpi
+#SBATCH --account=project_2000745
+#SBATCH --partition=large
+#SBATCH --time=00:05:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=16
+#SBATCH --reservation=summerschool
+
+srun ./mandelbrot

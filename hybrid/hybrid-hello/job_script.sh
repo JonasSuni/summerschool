06e34e02afff799cad70402527a23c43860c8e57
JonasSuni <jonas.suni@helsinki.fi>
2022-07-01 10:20:52 +0300

    test

--- hybrid/hybrid-hello/job_script.sh
+++ hybrid/hybrid-hello/job_script.sh
@@ -4,7 +4,7 @@
 #SBATCH --partition=gpu
 #SBATCH --time=00:05:00
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=1
+#SBATCH --ntasks=1
 #SBATCH --gres=gpu:v100:1
 #SBATCH --reservation=summerschool-gpu
 

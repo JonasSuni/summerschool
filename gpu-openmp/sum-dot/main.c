818e557ccb02c40ecb4dda83ef31218af9ecc573
JonasSuni <jonas.suni@helsinki.fi>
2022-07-01 13:29:42 +0300

    fix

--- gpu-openmp/sum-dot/main.c
+++ gpu-openmp/sum-dot/main.c
@@ -12,10 +12,11 @@ int main(void)
         vecB[i] = vecA[i] * vecA[i];
     }
 
+    double res;
 
     // TODO start: create a data region and offload the two computations
     // so that data is kept in the device between the computations
-    #pragma omp target data map(to:vecB[:]) map(tofrom:vecC[:])
+    #pragma omp target data map(to:vecB[:]) map(tofrom:vecC[:],res)
     {
     #pragma omp target map(to:vecA[:])
     #pragma omp parallel for
@@ -23,9 +24,9 @@ int main(void)
         vecC[i] = vecA[i] + vecB[i];
     }
 
-    double res = 0.0;
+    res = 0.0;
 
-    #pragma omp target map(tofrom:res)
+    #pragma omp target
     #pragma omp parallel for reduction(+:res)
     for (int i = 0; i < NX; i++) {
         res += vecC[i] * vecB[i];

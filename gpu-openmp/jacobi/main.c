8a6665232770dd8898d14f6b61a1b9f9cc7be74f
JonasSuni <jonas.suni@helsinki.fi>
2022-07-01 11:40:01 +0300

    test distribute for

--- gpu-openmp/jacobi/main.c
+++ gpu-openmp/jacobi/main.c
@@ -28,9 +28,9 @@ int main()
     // TODO start: offload the two stencil updates
 
     // Stencil update 1
-    #pragma omp target
-    #pragma omp loop
+    #pragma omp target teams distribute
     for (int i = 1; i < nx - 1; i++) {
+      #pragma omp parallel for
       for (int j = 1; j < ny - 1; j++) {
       int ind = i * ny + j;
       int ip = (i + 1) * ny + j;
@@ -43,9 +43,9 @@ int main()
     }
 
     // "Swap" the arrays, stencil update 2
-    #pragma omp target
-    #pragma omp loop
+    #pragma omp target teams distribute
     for (int i = 1; i < nx - 1; i++) {
+      #pragma omp parallel for
       for (int j = 1; j < ny - 1; j++) {
       int ind = i * ny + j;
       int ip = (i + 1) * ny + j;

7df0152b10787093c96f6dbfbda9c52e1cfa0cf0
JonasSuni <jonas.suni@helsinki.fi>
2022-07-02 11:28:32 +0300

    test

--- gpu-openmp/async-operations/cpp/mandelbrot.cpp
+++ gpu-openmp/async-operations/cpp/mandelbrot.cpp
@@ -30,7 +30,7 @@ int main() {
     int y_start = block * y_block_size;
     int y_end = y_start + y_block_size;
 
-    #pragma omp target teams distribute parallel for
+    #pragma omp target teams distribute parallel for depend(out:image[y_start]) nowait
     for (int y = y_start; y < y_end; y++) {
       for (int x = 0; x < width; x++) {
         int ind = y * width + x;
@@ -38,13 +38,13 @@ int main() {
       }
     }
 
+    #pragma omp target update from(image[block*block_size:(block+1)*block_size]) depend(in:image[y_start]) nowait
+
   }
   
   }
 
-  return 0;
-
-  // #pragma omp taskwait
+  #pragma omp taskwait
 
   // TODO end
 

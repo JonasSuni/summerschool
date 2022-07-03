e671804f5a4678635518b8cb62000b49089d57f9
JonasSuni <jonas.suni@helsinki.fi>
2022-07-03 14:34:08 +0300

    fix

--- gpu-hip/streams/main.cpp
+++ gpu-hip/streams/main.cpp
@@ -105,7 +105,7 @@ int main(int argc, char **argv)
 
     for (int i=0;i<nStreams;i++) {
       hipMemcpyAsync(&d_a[n*i/nStreams],&a[n*i/nStreams],bytes/nStreams,hipMemcpyHostToDevice,streams[i]);
-      hipLaunchKernelGGL(kernel,dim3(n/(blockSize*nStreams)),dim3(blockSize),0,streams[i],&d_a,n*i/nStreams);
+      hipLaunchKernelGGL(kernel,dim3(n/(blockSize*nStreams)),dim3(blockSize),0,streams[i],d_a,n*i/nStreams);
       hipMemcpyAsync(&a[n*i/nStreams],&d_a[n*i/nStreams],bytes/nStreams,hipMemcpyDeviceToHost,streams[i]);
     }
 

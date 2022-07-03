cae752854350c6eee7520da842c274ef8d6bd3e3
JonasSuni <jonas.suni@helsinki.fi>
2022-07-03 10:36:46 +0300

    end test

--- gpu-hip/hello-world/job_script.sh
+++ gpu-hip/hello-world/job_script.sh
@@ -4,7 +4,7 @@
 #SBATCH --partition=gpu
 #SBATCH --time=00:05:00
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=2
+#SBATCH --ntasks-per-node=1
 #SBATCH --gres=gpu:v100:2
 #SBATCH --reservation=summerschool-gpu
 

e7dba60734950b33742f6ba201cf07e244fe0e80
JonasSuni <jonas.suni@helsinki.fi>
2022-07-04 10:45:25 +0300

    test

--- gpu-hip/vector-sum/main.cpp
+++ gpu-hip/vector-sum/main.cpp
@@ -95,7 +95,7 @@ int main(int argc, char *argv[])
         hipMemcpyAsync(&dA[i],&hA[dec[i].start],dec[i].len*sizeof(double),hipMemcpyHostToDevice,strm[i]);
         hipMemcpyAsync(&dC[i],&hB[dec[i].start],dec[i].len*sizeof(double),hipMemcpyHostToDevice,strm[i]);
         hipLaunchKernelGGL(vector_add,dim3(10),dim3(ThreadsInBlock),0,strm[i],dC[i],dA[i],dB[i],dec[i].len);
-        hipMemcpyAsync(&hC[dec[i].start],&dC[i],dec[i].len*sizeof(double),hipMemcpyDeviceToHost,strm[i]);
+        hipMemcpyAsync(&hC[dec[0].start],&dC[i],dec[i].len*sizeof(double),hipMemcpyDeviceToHost,strm[i]);
     }
 
     // Synchronize and destroy the streams

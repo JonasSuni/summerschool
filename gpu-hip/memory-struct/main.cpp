7161c3e0ff4898b8349340ef39c7bb8bcc5e63bd
JonasSuni <jonas.suni@helsinki.fi>
2022-07-04 09:44:51 +0300

    test

--- gpu-hip/memory-struct/main.cpp
+++ gpu-hip/memory-struct/main.cpp
@@ -127,17 +127,19 @@ void freeDeviceExample(Example *d_ex)
 
   //#error Copy struct members (pointers) from device to host
 
-  hipMemcpy(d_x,(d_ex->x),size*sizeof(float),hipMemcpyDeviceToHost);
-  hipMemcpy(d_idx,(d_ex->idx),size*sizeof(int),hipMemcpyDeviceToHost);
+  hipMemcpy(&d_x,&(d_ex->x),sizeof(float*),hipMemcpyDeviceToHost);
+  hipMemcpy(&d_idx,&(d_ex->idx),sizeof(int*),hipMemcpyDeviceToHost);
   
 
   //#error Free device struct members
 
-  hipFree(d_x);
-  hipFree(d_idx);
+  free(d_x);
+  free(d_idx);
 
   //#error Free device struct
 
+  hipFree(d_ex->x);
+  hipFree(d_ex->idx);
   hipFree(d_ex);
 }
 

617ac9aafd4d8d94a655dae879306f266230c8ea
JonasSuni <jonas.suni@helsinki.fi>
2022-08-26 11:54:07 +0300

    fix

--- gpu-hip/memory-struct/job_ukko.sh
+++ gpu-hip/memory-struct/job_ukko.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=csc_gpu
 #SBATCH -M ukko
-#SBATCH --partition=gpu-short
+#SBATCH --partition=gpu
 #SBATCH --time=00:05:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1

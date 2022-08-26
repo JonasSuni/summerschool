df8f16a3073c8cb04238057ca538662f5dad4913
JonasSuni <jonas.suni@helsinki.fi>
2022-08-26 11:53:34 +0300

    Test job

--- gpu-hip/memory-struct/job_ukko.sh
+++ gpu-hip/memory-struct/job_ukko.sh
@@ -8,4 +8,11 @@
 #SBATCH --constraint=v100
 #SBATCH --gres=gpu:1
 
+module purge
+module load GCC/10.2.0
+module load CUDA/11.1.1-GCC-10.2.0
+module load CMake/3.18.4-GCCcore-10.2.0
+module use /proj/group/spacephysics/modules/hip
+module load hip-4.5
+
 srun ./main
